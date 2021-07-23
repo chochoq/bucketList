27e5020b080d7087a7212e4f9d1ae1d6311f9778
chochoq <aubepe@gmail.com>
2021-07-23 23:00:33 +0900

    (add) 비동기통신을위한 middleware thunk 적용

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -1,12 +1,17 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import bucket from "./modules/bucket";
 import { createBrowserHistory } from 'history';
 
 export const history = createBrowserHistory();
 
+const middleware = [thunk];
+
+const enhancer = applyMiddleware(...middleware);
+
 // 리듀서가 여러개일경우 컴바인리듀서를 통해서 함쳐줄수있다
 const rootReducer = combineReducers({ bucket });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer,enhancer);
 
 export default store;
