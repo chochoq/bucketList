8e81d8f673e933ece3af85a884e36780a0979bfb
chochoq <aubepe@gmail.com>
2021-07-25 01:31:46 +0900

    (style) button -> matelal UI로 변경

--- src/Detail.js
+++ src/Detail.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { updateBucketFB, deleteBucketFB } from './redux/modules/bucket';
-import styled from 'styled-components';
+
+import { Button, ButtonGroup } from '@material-ui/core';
+
+
 
 const Detail = (props) => {
     const dispatch = useDispatch();
+
+
+
 
     // -3) 상세페이지에서 버킷리스트 내용을 띄워보자
     const bucket_list = useSelector((state) => state.bucket.list);
@@ -16,26 +22,24 @@ const Detail = (props) => {
         <div>
             <h1>{bucket_list[bucket_index].text}</h1>
 
-            <Button onClick={() => {
-                dispatch(deleteBucketFB(bucket_index));
-                props.history.goBack();
-            }}>삭제</Button>
+            <ButtonGroup >
+                <Button
+                    style={{color: '#8b5599', border: '1px solid #8b5599'}}
+                    onClick={() => {
+                    dispatch(deleteBucketFB(bucket_index));
+                    props.history.goBack();
+                }}>삭제</Button>
 
-            <Button onClick={() => {
-                dispatch(updateBucketFB(bucket_index));
-                props.history.goBack();
-            }}>완료하기</Button>
+                <Button
+                    style={{color: '#8b5599', border: '1px solid #8b5599'}}
+                    onClick={() => {
+                    dispatch(updateBucketFB(bucket_index));
+                    props.history.goBack();
+                    }}>완료하기</Button>
+            </ButtonGroup>
         </div>
     )
 }
-
-const Button = styled.button`
-    width:25%;
-    background-color: #DCC2E2;
-    border: 1px solid #ddd0e0;
-    border-radius: 10px;
-    margin: 5% 12%;
-`;
 
 
 export default Detail;
